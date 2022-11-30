6e2ad59fd1ff04e332ac6117ef076e5f946c27df
samridht23 <samridht23@gmail.com>
2022-11-30 20:22:40 +0530

    updated

--- lua/configs/lsp/mason.lua
+++ lua/configs/lsp/mason.lua
@@ -1,5 +1,7 @@
 local status_ok, mason = pcall(require, "mason")
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
 -- set options here if required ,inside the setup function
 -- all default config loaded
--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,15 +1,16 @@
 local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
-if not status_ok then return end
+if not status_ok then
+	return
+end
 
 local options = {
-    ensure_installed = {"lua"},
-    highlight = {
-        enable = true,
-        use_languagetree = true,
-        additional_vim_regex_highlighting = true
-    },
-    autopairs = {enable = true},
-    indent = {enable = true}
+	ensure_installed = { "lua" },
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+		additional_vim_regex_highlighting = true,
+	},
+	autopairs = { enable = true },
+	indent = { enable = true },
 }
 treesitter.setup(options)
-
